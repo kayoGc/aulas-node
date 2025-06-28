805b17cba27e7a74cc675794c6005018b006b8d1
= <=>
2025-06-28 09:59:00 -0300

    intervalo

---
+++ aula04/mini_mvc/controllers/postController.js
@@ -0,0 +1,24 @@
+const postModel = require('./models/post');
+const fs = require('fs');
+const path = require('path');
+
+exports.listaPosts = async (req, res)  => {
+    const posts = await postModel.buscarPosts();
+
+    fs.readFile(path.join(__dirname, '../views/index.html'), 'utf8', (err, html) => {
+        if (err) {
+            return res.status(500)
+                .send('Erro ao carregar a view');
+        };
+
+        const conteudo = posts.slice(0, 10).map(post => `
+            <div>
+                <h2>${post.title}</h2>
+                <p>${post.body}</p>
+            </div>
+        `).join('');
+    ''
+        const pagina = html.replace('{{conteudo}}', conteudo);
+        res.send(pagina);
+    });
+};
